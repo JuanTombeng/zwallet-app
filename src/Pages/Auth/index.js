cf68acbb9c1ec87fceca48a9085b06506c342e29
Juan Tombeng <juantombeng03@gmail.com>
2022-02-24 02:48:26 +0700

    boiler plate

---
+++ src/Pages/Auth/index.js
@@ -0,0 +1,23 @@
+import React, { useState, Fragment } from "react"
+import { useNavigate, Link } from "react-router-dom"
+
+
+
+const Signup = () => {
+    return (
+        <div className="Signup d-flex">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+
+                    </div>
+                    <div className="col">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
